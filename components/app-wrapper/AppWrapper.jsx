3151f3a46bb9127c47c69da9d192625078ca6fe6
Lymar Paul Paller <lymar.paller101@gmail.com>
2023-11-01 20:23:20 +0800

    refactor: added component for back end and front end wrapper

---
+++ components/app-wrapper/AppWrapper.jsx
@@ -0,0 +1,11 @@
+function AppWrapper ({ imgSvg }) {
+    return (
+        <>
+                <div className="tech-stack-circle">
+                    {imgSvg}
+                </div>
+        </>
+    )
+}
+
+export default AppWrapper
--- pages/web-dev/WebDev.jsx
+++ pages/web-dev/WebDev.jsx
@@ -12,8 +12,51 @@ import SqlSvg from "../../components/svg/techStackSvg/SqlSvg";
 import JavaScriptSvg from "../../components/svg/techStackSvg/JavaScriptSvg";
 import kairosImg from "../../images/kairos-sample.png";
 import nexusImg from "../../images/nexus-sample.png";
+import AppWrapper from "../../components/app-wrapper/AppWrapper";
 
 function WebDev () {
+    const frontEndArray = [
+        {
+            id: 1,
+            svg: <HtmlSvg/>
+        },
+        {
+            id: 2,
+            svg: <CssSvg/>
+        },
+        {
+            id: 3,
+            svg: <SassSvg/>
+        },
+        {
+            id: 4,
+            svg: <BootstrapSvg/>
+        },
+        {
+            id: 5,
+            svg: <JavaScriptSvg/>
+        },
+        {
+            id: 6,
+            svg: <ReactJsSvg/>
+        },
+    ]
+
+    const backEndArray = [
+        {
+            id: 1,
+            svg: <PhpSvg/>
+        },
+        {
+            id: 2,
+            svg: <LaravelSvg/>
+        },
+        {
+            id: 3,
+            svg: <SqlSvg/>
+        }
+    ]
+
     return (
         <>
             <div className="webdev-main-container">
@@ -34,40 +77,28 @@ function WebDev () {
                         <div className="front-end-container circle-container">
                             <h2>Front End</h2>
                             <div className="circle-wrapper">
-                                <div className="tech-stack-circle">
-                                    <HtmlSvg/>
-                                </div>
-                                <div className="tech-stack-circle">
-                                    <CssSvg/>
-                                </div>
-                                <div className="tech-stack-circle">
-                                    <SassSvg/>
-                                </div>
-                                <div className="tech-stack-circle">
-                                    <JavaScriptSvg/>
-                                </div>
-                                <div className="tech-stack-circle">
-                                    <BootstrapSvg/>
-                                </div>
-                                <div className="tech-stack-circle">
-                                    <ReactJsSvg/>
-                                </div>
+                                {
+                                    frontEndArray.map(frontEndARR =>
+                                        <AppWrapper
+                                        key={frontEndARR.id}
+                                        imgSvg={frontEndARR.svg}
+                                        />
+                                    )
+                                }
                             </div>
                         </div>
 
                         <div className="back-end-container circle-container">
                             <h2>Back End</h2>
                             <div className="circle-wrapper">
-                                <div className="tech-stack-circle">
-                                    <PhpSvg/>
-                                </div>
-                                <div className="tech-stack-circle">
-                                    <LaravelSvg/>
-                                </div>
-                                <div className="tech-stack-circle">
-                                    <SqlSvg/>
-                                </div>
-
+                                {
+                                    backEndArray.map(backEndARR =>
+                                    <AppWrapper
+                                        key={backEndARR.id}
+                                        imgSvg={backEndARR.svg}
+                                    />
+                                    )
+                                }
                             </div>
                         </div>
                     </div>
